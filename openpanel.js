1792e553f72ae7d3bc3f3818c0d5cd14c9c8758f
Konstantin Vinogradov <konstantin@endowment.dev>
2025-04-24 12:03:14 +0100

    Delete openpanel.js

--- openpanel.js
+++
@@ -1,7 +0,0 @@
-window.op = window.op||function(...args){(window.op.q=window.op.q||[]).push(args);};
-window.op('init', {
-  clientId: '81e37e5e-5c41-47bb-a7a7-8772f5f45a4c',
-  trackScreenViews: true,
-  trackOutgoingLinks: true,
-  trackAttributes: true,
-});
