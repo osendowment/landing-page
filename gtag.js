648e20065e72c4e429e31377d614af70ba3bf43e
Konstantin Vinogradov <konstantin@endowment.dev>
2025-05-01 12:20:51 +0100

    Create gtag.js

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XE63Z4PW8X"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-XE63Z4PW8X');
+</script>
