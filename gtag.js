7feff634426a18a903a6357f8eb8d6776cbb3e5a
Konstantin Vinogradov <konstantin@endowment.dev>
2025-05-01 12:30:40 +0100

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -1,8 +1,4 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-XE63Z4PW8X"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-XE63Z4PW8X');
-</script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XE63Z4PW8X');
